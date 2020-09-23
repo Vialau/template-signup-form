1683a6ca0bfac686a0f9c8a7366fa32d89c239c6
Vialau <vianney.lau@gmail.com>
2020-09-23 14:18:10 +0200

    add delete method

--- main.js
+++ main.js
@@ -23,4 +23,23 @@ $(document).ready(function(){
 	  }
 	});
   });
+
+  $('#delete').submit(function(e) {
+	e.preventDefault();
+	const id=$('#id').val();
+	var myHeaders = new Headers();
+	myHeaders.append("Cookie", "__cfduid=de5323248a6fe2a2af9cd5bb8f82696df1600862832");
+	
+	var requestOptions = {
+	  method: 'DELETE',
+	  headers: myHeaders,
+	  redirect: 'follow'
+	};
+	
+	fetch(`https://api.sheety.co/8647c1294e14cf39f72566bf52df77ba/signupForm/emails/${id}`, requestOptions)
+	  .then(response => response.text())
+	  .then(result => console.log(result))
+	  .catch(error => console.log('error', error));
+
+	});
 });
